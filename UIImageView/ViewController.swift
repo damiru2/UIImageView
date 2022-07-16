40216ac2c99d6fcbcac1122a7e7b0c02c789b5ee
damiru2 <chalkarov11@mail.ru>
2022-07-16 19:44:02 +0500

    Initial Commit

---
+++ UIImageView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIImageView
+//
+//  Created by Damir Chalkarov on 16.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
